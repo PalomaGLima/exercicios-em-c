7fa976fc00641d17762306643d819fcc76c5b3a4
Paloma <palomalima272@gmail.com>
2023-04-11 18:20:07 -0300

    02

---
+++ questão2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+    float n;
+    printf("digite um numero:\n");
+    scanf("%f", &n);
+
+    printf("o numero informado foi:%.2f",n);
+
+    return 0;
+}
